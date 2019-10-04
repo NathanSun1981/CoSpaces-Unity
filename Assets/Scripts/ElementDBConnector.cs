e5c2aaf9e7331c41c74259975d4bed13c8c93299
Matthew Plaudis <matthew.plaudis@mail.utoronto.ca>
2019-10-04 09:06:43 -0700

    run in Windows platform

--- Assets/Scripts/ElementDBConnector.cs
+++ Assets/Scripts/ElementDBConnector.cs
@@ -11,6 +11,10 @@ public class ElementDBConnector : MonoBehaviour
     // Start is called before the first frame update
 
     public Text text;
+    private int currentIndex;
+    private int indexs;
+    private bool bStop = false;
+
     void Start()
     {
         ResourceHandler.Instance.Handlers.Clear();
@@ -18,14 +22,15 @@ public class ElementDBConnector : MonoBehaviour
         ResourceHandler.Instance.RegisterHandler(new IsoModelHandler());
         ResourceHandler.Instance.RegisterHandler(new MassingDaeGenerator());
         ResourceHandler.Instance.RegisterHandler(new HDDaeGenerator());
-        StartCoroutine(Resourcehaddlling());
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (bStop)
+            text.text = string.Format("Processing Stop at Case {0}", currentIndex);
+        else
+            text.text = string.Format("Loading Cases ({0} of {1})...", currentIndex, indexs);
     }
 
     private IEnumerator Resourcehaddlling()
@@ -38,19 +43,43 @@ public class ElementDBConnector : MonoBehaviour
         int count = 0;
         foreach (DBCase c in cases)
         {
-            count++;
-            text.text = string.Format("Loading Cases ({0} of {1})...", count, cases.Count);
-            DBCase cc = Elements.GetCaseByID(c._CaseId);
+            if (!bStop)
+            {
+                count++;
+                currentIndex = count;
+                indexs = cases.Count;
 
-            string warehoused = "";
-            cc.Properties.TryGetValue("is_warehoused", out warehoused);
-            if (warehoused == "1")
-                continue;
-            ResourceHandler.Instance.HandleCase(cc, cc.ResourceList, "./Cache",
-                "case-resources", cc._CaseNumber, false);
+           
+                DBCase cc = Elements.GetCaseByID(c._CaseId);
+
+                string warehoused = "";
+                cc.Properties.TryGetValue("is_warehoused", out warehoused);
+                if (warehoused == "1")
+                    continue;
+                ResourceHandler.Instance.HandleCase(cc, cc.ResourceList, "./Cache",
+                    "case-resources", cc._CaseNumber, false);
+            }          
             yield return null;
         }
 
     }
 
+    public void OnClickDownloading()
+    {
+        bStop = false;
+        StartCoroutine(Resourcehaddlling());
+    }
+
+
+    public void OnClickExit()
+    {
+        StopCoroutine(Resourcehaddlling());
+        Application.Quit();
+    }
+
+    public void OnClickStop()
+    {
+        bStop = true;
+        StopCoroutine(Resourcehaddlling());
+    }
 }
